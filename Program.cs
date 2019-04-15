51920169a58d9e9e7f71a1aabe564b542e8d3b85
YeJuneJi <darksuhak7@gmail.com>
2019-04-15 18:07:36 +0900

    BambooExam First

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenFibonacci(); 
+        }
+
+        private static void EvenFibonacci()
+        {
+            int x = 1;
+            int y = 2;
+            int sum = 0;
+            Console.Write("Input >>");
+            int inputNum = int.Parse(Console.ReadLine());
+
+            while (x < inputNum)
+            {
+                if (x % 2 == 0)
+                {
+                    sum = sum + x;
+                }
+                int z = x + y;
+                x = y;
+                y = z;
+
+            }
+            Console.WriteLine("Output : {0}", sum);
+        }
+    }
+}
