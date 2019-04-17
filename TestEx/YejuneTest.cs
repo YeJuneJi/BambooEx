82f2897dd062f6bb77762cdc98cb07ac5c0f6db5
YeJuneJi <darksuhak7@gmail.com>
2019-04-17 16:06:21 +0900

    AL Cm

---
+++ TestEx/YejuneTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestEx
+{
+    [TestClass]
+    public class YejuneTest
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Ex.YejuneEx2 ex = new Ex.YejuneEx2();
+            Assert.AreEqual(31, ex.Add(20, 11));
+        }
+
+        [TestMethod]
+        public void Sub()
+        {
+            Ex.YejuneEx2 ex = new Ex.YejuneEx2();
+            Assert.AreEqual(10, ex.Sub(20, 10));
+        }
+    }
+}
